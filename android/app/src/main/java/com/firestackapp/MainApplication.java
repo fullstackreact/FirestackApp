8990af117f7a00061ba33714f76219195881a097
Ari Lerner <arilerner@mac.com>
2016-09-30 04:09:21 -0700

    Fixed android app

--- android/app/src/main/java/com/firestackapp/MainApplication.java
+++ android/app/src/main/java/com/firestackapp/MainApplication.java
@@ -5,9 +5,6 @@ import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import io.fullstack.firestack.FirestackPackage;
-import io.fullstack.firestack.FirestackPackage;
-import io.fullstack.firestack.FirestackPackage;
-import io.fullstack.firestack.FirestackPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -29,9 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FirestackPackage(getApplicationContext()),
-            new FirestackPackage(getApplicationContext()),
-            new FirestackPackage(getApplicationContext()),
             new FirestackPackage(getApplicationContext()),
             new VectorIconsPackage()
       );
