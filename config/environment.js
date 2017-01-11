712a2e3762bc9e003e32d446d667dfb8d7f16140
Ari Lerner <arilerner@mac.com>
2017-01-11 10:41:37 -0800

    Merge platform config in environment

--- config/environment.js
+++ config/environment.js
@@ -1,10 +1,17 @@
+import { Platform } from 'react-native';
+
 let defaultEnv = {
   NAME: 'DoGood'
 }
 
+const NODE_ENV = process.env.NODE_ENV;
 let env = {}
-if (process.env.NODE_ENV === 'development') {
+if (NODE_ENV === 'development') {
   env = require('./development')
 }
+
+try {
+  env = Object.assign({}, env, require(`${NODE_ENV}.${Platform.OS}`));
+} catch (e) {}
 
 module.exports = Object.assign({}, defaultEnv, env);
